dfeb5283358a1c0b420089d22e7eeba66ca56ef7
Mitran Matei <m.mitran@student.fontys.nl>
2021-04-21 19:05:32 +0300

    Classes for the warehouse manager

---
+++ PRJMediaBazaar/PRJMediaBazaar/Data/ItemDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using PRJMediaBazaar.Logic;
+
+namespace PRJMediaBazaar.Data
+{
+    class ItemDAL : BaseDAL
+    {
+        public /*List<Item>*/ void SelectAll()
+        {
+
+        } 
+        public bool AddItem(int id, String name, String category, String subcategory, int price, int quantity)
+        {
+            CloseConnection();
+            return true;
+        }
+        public bool DeleteItem(int id)
+        {
+            CloseConnection();
+            return true;
+        }
+        public bool UpdateItem(int id)
+        {
+            CloseConnection();
+            return true;
+        }
+    }
+}
---
+++ PRJMediaBazaar/PRJMediaBazaar/Logic/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRJMediaBazaar.Logic
+{
+    public class Item
+    {
+        public Item(int id, String name, String category, String subcategory, double price, int quantity)
+        {
+            ID = id;
+            Name = name;
+            Category = category;
+            Subcategory = subcategory;
+            Price = price;
+            Quantity = quantity;
+        }
+        public void SetSpecification(Specification spec)
+        {
+            Specifications.Add(spec);
+        }
+       public int ID { get; private set; }
+       public String Name { get; private set; }
+       public String Category { get; private set; }
+       public String Subcategory { get; private set; }
+       public double Price { get; private set; }
+       public int Quantity { get; private set; }
+       public List<Specification> Specifications { get; private set; }
+       public double TotalPrice { get { return this.Price * this.Quantity; } }
+    }
+}
